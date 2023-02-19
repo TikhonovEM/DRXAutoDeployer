7abbd5c1587bc8d83a6ddc1d2644d4bd07fa3014
Ququsick <sell76770@gmail.com>
2023-02-19 14:59:37 +0400

    Реализация варианта Add работы с метками для agile-досок

--- DirectumRXAutoDeployer/Notifiers/AgileBoards/ActionHandlers/TagActionHandler.cs
+++ DirectumRXAutoDeployer/Notifiers/AgileBoards/ActionHandlers/TagActionHandler.cs
@@ -11,6 +11,7 @@ using System.Text;
 using Microsoft.OData.Client;
 using Microsoft.OData.Extensions.Client;
 using Sungero.IntegrationService.Models.Generated.ProjectRequirement;
+using Newtonsoft.Json;
 
 namespace DirectumRXAutoDeployer.Notifiers.AgileBoards.ActionHandlers
 {
@@ -33,6 +34,7 @@ namespace DirectumRXAutoDeployer.Notifiers.AgileBoards.ActionHandlers
         private readonly AgileBoardSettings _agileBoardsSettings;
         private readonly ActionSetting _action;
         private readonly List<TicketTagPair> _ticketTagsToRemove = new List<TicketTagPair>();
+        private readonly List<int> _ticketsToAddTag = new List<int>();
 
         public TagActionHandler(ILogger logger, Container client, AgileBoardSettings agileBoardsSettings, ActionSetting action)
         {
@@ -43,6 +45,14 @@ namespace DirectumRXAutoDeployer.Notifiers.AgileBoards.ActionHandlers
         }
         
         public async Task HandleStartAsync()
+        {
+            if (_action.Type == TagActionType.Remove)
+                await HandleStartRemoveActionAsync();
+            else
+                await HandleStartAddActionAsync();
+        }
+
+        private async Task HandleStartRemoveActionAsync()
         {
             var columnFrom = (await _client.IColumns
                     .Expand("Tickets($expand=Ticket($expand=TicketsTags($expand=TicketTag)))")
@@ -58,7 +68,9 @@ namespace DirectumRXAutoDeployer.Notifiers.AgileBoards.ActionHandlers
             }
 
             var ticketTagsToRemove = columnFrom.Tickets.SelectMany(t =>
-                t.Ticket.TicketsTags.Where(tt => tt?.TicketTag?.Name == _action.TagName).Select(tt => new TicketTagPair(t.Ticket.Id, tt.Id))).ToList();
+                t.Ticket.TicketsTags.Where(tt => tt?.TicketTag?.Name == _action.TagName)
+                    .Select(tt => new TicketTagPair(t.Ticket.Id, tt.Id)))
+                .ToList();
 
             if (!ticketTagsToRemove.Any())
             {
@@ -69,11 +81,37 @@ namespace DirectumRXAutoDeployer.Notifiers.AgileBoards.ActionHandlers
             _ticketTagsToRemove.AddRange(ticketTagsToRemove);
         }
 
+        private async Task HandleStartAddActionAsync()
+        {
+            var columnFrom = (await _client.IColumns
+                    .Expand("Tickets($expand=Ticket($expand=TicketsTags($expand=TicketTag)))")
+                    .Where(c => c.Name == _action.ColumnFrom &&
+                                c.BoardId == _agileBoardsSettings.BoardId)
+                    .ExecuteAsync<IColumnDto>())
+                .FirstOrDefault();
+            
+            if (columnFrom == null)
+            {
+                _logger.LogError("TagActionHandler. Column with name '{0}' not found",  _agileBoardsSettings.ColumnFrom);
+                return;
+            }
+
+            var ticketsToAddTag = columnFrom.Tickets.Where(t => t.Ticket != null && 
+                                                                t.Ticket.TicketsTags.All(tt => tt?.TicketTag?.Name != _action.TagName))
+                .Select(t => t.Ticket.Id)
+                .ToList();
+
+            if (!ticketsToAddTag.Any())
+            {
+                _logger.LogWarning("TagActionHandler. There are not tickets to add tag '{0}' in column '{1}'",  _action.TagName, _action.ColumnFrom);
+                return;
+            }
+            
+            _ticketsToAddTag.AddRange(ticketsToAddTag);
+        }
+
         public async Task HandleFinishAsync()
         {
-            if (!_ticketTagsToRemove.Any())
-                return;
-            
             // В общем я пока не разобрался, как через ODataClient удалить записи в коллекции, поэтому буду слать обычные http-запросы.
 
             using var httpClient = new HttpClient();
@@ -81,6 +119,17 @@ namespace DirectumRXAutoDeployer.Notifiers.AgileBoards.ActionHandlers
             
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
                 Convert.ToBase64String(Encoding.ASCII.GetBytes(_agileBoardsSettings.Token)));
+            
+            if (_action.Type == TagActionType.Remove)
+                await HandleFinishRemoveActionAsync(httpClient);
+            else
+                await HandleFinishAddActionAsync(httpClient);
+        }
+
+        private async Task HandleFinishRemoveActionAsync(HttpClient httpClient)
+        {
+            if (!_ticketTagsToRemove.Any())
+                return;
 
             const string deleteUriTemplate = "ITickets({0})/TicketsTags({1})";
             var successfulRemovedIds = new List<int>();
@@ -101,6 +150,44 @@ namespace DirectumRXAutoDeployer.Notifiers.AgileBoards.ActionHandlers
             }
             
             _logger.LogInformation("TagActionHandler. Tag successfully removed from tickets {0}", string.Join(";", successfulRemovedIds));
+        }
+
+        private async Task HandleFinishAddActionAsync(HttpClient httpClient)
+        {
+            if (!_ticketsToAddTag.Any())
+                return;
+
+            var tag = (await _client.ITags.Where(t => t.Name == _action.TagName).ExecuteAsync<ITagDto>())
+                .FirstOrDefault();
+
+            if (tag == null)
+            {
+                _logger.LogWarning("TagActionHandler. Tag with name '{0}' not found in database", _action.TagName);
+                return;
+            }
+
+            const string addUriTemplate = "ITickets({0})/TicketsTags";
+            var successfulAddedIds = new List<int>();
+            foreach (var ticketId in _ticketsToAddTag)
+            {
+                try
+                {
+                    var addUri = string.Format(addUriTemplate, ticketId);
+                    var ticketIdOObject = new { Id = ticketId };
+                    var tagIdObject = new { Id = tag.Id };
+                    var requestObject = new { Ticket = ticketIdOObject, TicketTag = tagIdObject };
+                    var request = new StringContent(JsonConvert.SerializeObject(requestObject), Encoding.UTF8, "application/json");
+                    var response = await httpClient.PostAsync(addUri, request);
+                    if (response.IsSuccessStatusCode)
+                        successfulAddedIds.Add(ticketId);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogWarning("TagActionHandler. Cannot add tag to ticket {0}", ticketId);
+                }
+            }
+            
+            _logger.LogInformation("TagActionHandler. Tag successfully added to tickets {0}", string.Join(";", successfulAddedIds));
         }
     }
 }
